65f950791adb2b9e850f33ab1608db4d9f59ca77
Ayush @ Classiebit <ayush.classiebit@gmail.com>
2023-06-29 16:53:16 +0530

    chore: setup initial layout & page

---
+++ app/(site)/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import AuthForm from "./components/AuthForm";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image alt="logo" src="/images/logo.png" width={48} height={48} className="mx-auto w-auto" />
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+      </div>
+      <AuthForm />
+    </div>
+  );
+}
