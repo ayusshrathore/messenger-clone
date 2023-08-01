890d59712938b2c83f33babd7b8792db25362509
ayusshrathore <ayusshrathore@gmail.com>
2023-08-01 20:23:44 +0530

    feat: add component to render avatar & empty state

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,11 @@
+const EmptyState = () => {
+  return (
+    <div className="px-4 py-10 sm:px-6 lg:px-8 h-full flex items-center justify-center bg-gray-100">
+      <div className="text-center items-center flex flex-col">
+        <h3 className="mt-2 text-2xl font-semibold text-gray-500">Select a chat or start a new conversation</h3>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyState;
