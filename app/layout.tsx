ade56c4222775619a1e1380717251ca375b214a5
ayusshrathore <ayusshrathore@gmail.com>
2023-08-01 20:24:47 +0530

    feat: render toaster provider & add auth context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
 import { Inter } from "next/font/google";
+
+import AuthContext from "./context/AuthContext";
+import ToasterContext from "./context/ToasterContext";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
+      </body>
     </html>
   );
 }
