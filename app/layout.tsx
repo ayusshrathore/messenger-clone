5a7fc9cccc366aff80daf0b7018cdfee4c5d8f2b
ayusshrathore <ayusshrathore@gmail.com>
2023-08-03 09:34:49 +0530

    chore: render active status

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import AuthContext from "./context/AuthContext";
 import ToasterContext from "./context/ToasterContext";
 
+import ActiveStatus from "./components/ActiveStatus";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
         <AuthContext>
           <ToasterContext />
+          <ActiveStatus />
           {children}
         </AuthContext>
       </body>
