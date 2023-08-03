6848d89378b97e1c9bea08d8101ff2be4950f83e
ayusshrathore <ayusshrathore@gmail.com>
2023-08-03 09:33:06 +0530

    chore: add store & hook to fetch active members

---
+++ app/hooks/useActiveChannel.ts
@@ -0,0 +1,42 @@
+import { Channel, Members } from "pusher-js";
+import { useEffect, useState } from "react";
+import { pusherClient } from "../libs/pusher";
+import useActiveList from "./useActiveList";
+
+const useActiveChannel = () => {
+  const { set, remove, add } = useActiveList();
+  const [activeChannel, setActiveChannel] = useState<Channel | null>(null);
+
+  useEffect(() => {
+    let channel = activeChannel;
+
+    if (!channel) {
+      channel = pusherClient.subscribe("presence-messenger");
+      setActiveChannel(channel);
+    }
+
+    channel.bind("pusher:subscription_succeeded", (members: Members) => {
+      const initialMembers: string[] = [];
+
+      members.each((member: Record<string, any>) => initialMembers.push(member.id));
+      set(initialMembers);
+    });
+
+    channel.bind("pusher:member_added", (member: Record<string, any>) => {
+      add(member.id);
+    });
+
+    channel.bind("pusher:member_removed", (member: Record<string, any>) => {
+      remove(member.id);
+    });
+
+    return () => {
+      if (activeChannel) {
+        pusherClient.unsubscribe("presence-messenger");
+        setActiveChannel(null);
+      }
+    };
+  }, [activeChannel, add, remove, set]);
+};
+
+export default useActiveChannel;
