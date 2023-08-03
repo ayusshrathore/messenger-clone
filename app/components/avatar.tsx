e789fd242c7e16aeb728f95847dde6b294ebe085
ayusshrathore <ayusshrathore@gmail.com>
2023-08-03 19:22:06 +0530

    chore: update avatar component

--- app/components/avatar.tsx
+++ app/components/avatar.tsx
@@ -6,21 +6,27 @@ import { FC } from "react";
 import useActiveList from "../hooks/useActiveList";
 
 interface AvatarProps {
-  user?: User;
+	user?: User;
 }
 
 const Avatar: FC<AvatarProps> = ({ user }) => {
-  const { members } = useActiveList();
-  const isActive = members.indexOf(user?.email!) !== -1;
+	const { members } = useActiveList();
+	const isActive = members.indexOf(user?.email!) !== -1;
 
-  return (
-    <div className="relative">
-      <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
-        <Image alt="Avatar" src={user?.image || "/images/placeholder.jpg"} fill />
-      </div>
-      {isActive && <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3" />}
-    </div>
-  );
+	return (
+		<div className="relative">
+			<div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
+				<Image
+					alt="Avatar"
+					src={user?.image || "/images/placeholder.jpg"}
+					fill
+				/>
+			</div>
+			{isActive && (
+				<span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3" />
+			)}
+		</div>
+	);
 };
 
 export default Avatar;
